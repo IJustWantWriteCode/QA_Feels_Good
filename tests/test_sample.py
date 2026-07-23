5081bb5ab0be9c51d1b36a147a54730d9038c549
Maxim_Erin <maxim.erin@proton.me>
2026-07-23 16:15:14 +0300

    feat: add fixtures in 'test_samples' and 'conftest'

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,2 +1,37 @@
-def test_math():
-    assert 2 + 2 == 4
+import pytest
+from datetime import datetime
+
+
+@pytest.fixture(scope="module")
+def start_tests():
+    timestamp = datetime.now().strftime("%H:%M:%S.%f")[:-3]
+    db_state = {"status_type": "connected", "data": [1, 2, 3]}
+    print(
+        f"\n[{timestamp}]Начат прогон тестов из файла test_sample.py. "
+        f"Состояние: {db_state}"
+    )
+    yield
+    db_state["status_type"] = "disconnected"
+    db_state["data"].clear()
+    timestamp_end = datetime.now().strftime("%H:%M:%S.%f")[:-3]
+    print(
+        f"\n[{timestamp_end}]Прогон тестов из файла test_sample.py окончен. "
+        f"Состояние: {db_state}"
+    )
+
+
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    print("Тест начат")
+    yield
+    print("\nТест окончен")
+
+
+def test_sample_1(start_tests, random_int_for_test):
+    a, b = random_int_for_test
+    assert a + b == b + a
+
+
+def test_sample_2(random_int_for_test):
+    a, b = random_int_for_test
+    assert not a - b == b - a
