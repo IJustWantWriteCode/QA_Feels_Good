9c5a420398bff066acf87a5176ee19fad8dc7c26
Maxim_Erin <maxim.erin@proton.me>
2026-07-24 16:38:21 +0300

    feat: add parametrized test with custom smoke mark and failure details

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -8,7 +8,8 @@ def start_tests():
     db_state = {"status_type": "connected", "data": [1, 2, 3]}
     print(
         f"\n[{timestamp}]Начат прогон тестов из файла test_sample.py. "
-        f"Состояние: {db_state}"
+        f"Состояние: {db_state['status_type']}. "
+        f"Данные: {db_state['data']}"
     )
     yield
     db_state["status_type"] = "disconnected"
@@ -16,7 +17,8 @@ def start_tests():
     timestamp_end = datetime.now().strftime("%H:%M:%S.%f")[:-3]
     print(
         f"\n[{timestamp_end}]Прогон тестов из файла test_sample.py окончен. "
-        f"Состояние: {db_state}"
+        f"Состояние: {db_state['status_type']}. "
+        f"Данные: {db_state['data']}"
     )
 
 
@@ -24,7 +26,7 @@ def start_tests():
 def run_around_tests():
     print("Тест начат")
     yield
-    print("\nТест окончен")
+    print("\nТест окончен\n")
 
 
 def test_sample_1(start_tests, random_int_for_test):
@@ -35,3 +37,41 @@ def test_sample_1(start_tests, random_int_for_test):
 def test_sample_2(random_int_for_test):
     a, b = random_int_for_test
     assert not a - b == b - a
+
+
+@pytest.mark.parametrize(
+    "percent_crit, damage",
+    [
+        (1.25, 55),
+        (1.07, 23),
+    ],
+    ids=["high_level_damage", "low_level_damage"],
+)
+@pytest.mark.parametrize(
+    "bonus_armor, armor",
+    [
+        (1.25, 44),
+        (1.10, 34),
+    ],
+    ids=["high_level_armor", "low_level_armor"],
+)
+@pytest.mark.smoke
+@pytest.mark.xfail(
+    strict=False, reason="Урон может быть недостаточным в некоторых комбинациях"
+)
+def test_damage_and_armor(percent_crit, damage, bonus_armor, armor):
+
+    total_damage = round(percent_crit * damage, 2)
+    total_armor = round(bonus_armor * armor, 2)
+
+    if total_damage > total_armor:
+        message = f"Успех: Урон ({total_damage}) пробил броню ({total_armor})"
+    elif total_damage < total_armor:
+        message = (
+            f"Провал: Броня ({total_armor}) оказалась больше урона ({total_damage})"
+        )
+    else:
+        message = f"Провал: Урон ({total_damage}) равен броне ({total_armor})"
+
+    print(f"\n[РЕЗУЛЬТАТ]: {message}")
+    assert total_damage > total_armor, message
