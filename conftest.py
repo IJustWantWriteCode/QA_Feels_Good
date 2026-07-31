f897edeec36980dc13d20062a7820141431c9223
Maxim_Erin <maxim.erin@proton.me>
2026-07-31 14:54:17 +0300

    feat: add new fixture for ui tests

--- conftest.py
+++ conftest.py
@@ -1,8 +1,18 @@
 import pytest
 import random
-
+from playwright.sync_api import sync_playwright
 
 @pytest.fixture()
 def random_int_for_test():
     a, b = random.randint(1, 9), random.randint(1, 9)
     return a, b
+
+@pytest.fixture(scope="function")
+def page():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        yield page
+        context.close()
+        browser.close()
