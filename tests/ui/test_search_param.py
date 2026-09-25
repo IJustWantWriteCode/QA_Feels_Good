122defcabe16121bba057f1496858e6a2354239b
Maxim_Erin <maxim.erin@proton.me>
2026-09-25 20:34:38 +0300

    ci: add full pipeline

--- tests/ui/test_search_param.py
+++ tests/ui/test_search_param.py
@@ -1,3 +1,5 @@
+import re
+
 import allure
 import pytest
 from playwright.sync_api import Page, expect
@@ -5,16 +7,16 @@ from playwright.sync_api import Page, expect
 
 @pytest.mark.parametrize("query", ["qa", "aqa", "python"])
 @pytest.mark.ui
-def test_duckduckgo_search_parametrized(page: Page, query: str) -> None:
-    with allure.step("Переход на страницу поискового сервиса 'duckduckgo'"):
-        page.goto("https://duckduckgo.com/")
+def test_wikipedia_search_parametrized(page: Page, query: str) -> None:
+    with allure.step("Переход на Главную страницу Wikipedia"):
+        page.goto("https://en.wikipedia.org/")
 
-    with allure.step("Ищем поисковую строку, заполняем данными и нажимаем 'Поиск'"):
-        search_input = page.get_by_role("combobox", name="Поиск в DuckDuckGo")
+    with allure.step("Ищем поисковую строку и вводим запрос"):
+        search_input = page.get_by_role("searchbox", name="Search Wikipedia")
         search_input.fill(query)
         search_input.press("Enter")
-    with allure.step("Проверка отображения результатов на странице"):
-        results = page.get_by_test_id("result")
-        expect(results.first).to_be_visible(timeout=10000)
 
-    assert results.count() >= 5
+    with allure.step("Проверка отображения основного заголовка статьи"):
+        main_heading = page.get_by_role("heading", level=1)
+        expect(main_heading).to_be_visible(timeout=10000)
+        expect(main_heading).to_contain_text(re.compile(query, re.IGNORECASE))
